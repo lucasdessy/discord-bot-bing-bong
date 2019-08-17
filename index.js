97fdea424e40d005188e88d2db0899030fc79084
lucasdessy <43278706+lucasdessy@users.noreply.github.com>
2019-08-17 12:24:36 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 client.on('ready', () =>{
     console.log('O bot está online');
     client.user.setActivity('bing bong');
@@ -10,6 +9,7 @@ const prefix = 'bong_'
 var bing_bong = ['bing', 'bong'];
 var proibido = 1;
 var admin_id = 342663595626201089;
+var user_id = null;
 client.on('message', (message) =>{
 
     if (message.author.bot) return;
@@ -17,25 +17,26 @@ client.on('message', (message) =>{
     //if (message.author.id == admin_id){
     if (message.member.roles.some(role => role.hasPermission("ADMINISTRATOR"))){
         
-        msg = message.content.toLowerCase;
-
+        msg = message.content.toLowerCase();
+        
         if(msg.startsWith(prefix)){
             if((msg.startsWith(prefix+'start')) && (mencao != null)){
                 proibido = 0;
                 user_id = mencao.id;
-                message.channel.send('Bong startado para o usuario com o id: '+mencao.id);        
+                message.channel.send('Bong startado para o usuario <@'+user_id+'>');
             }
             else if (msg == prefix+'start'){
                 message.channel.send('Erro: precisa @mencionar usuário.');
             }            
             else if(msg == prefix+'stop'){
                 proibido = 1;
+                user_id = null;
                 message.channel.send('Bong parado.');
             }
-            else if(msg == prefix+'status' && mencao != null){
-                message.channel.send('Bong startado para o usuario com o id: '+mencao.id);
+            else if(msg == prefix+'status' && user_id != null){
+                message.channel.send('Bong startado para o usuario <@'+user_id+'>');
             }
-            else if(msg == prefix+'status' && mencao == null){
+            else if(msg == prefix+'status' && user_id == null){
                 message.channel.send('Bong está parado.')
             }
             else{
@@ -65,3 +66,4 @@ client.on('message', (message) =>{
 })
 
 client.login(process.env.token);
+//client.login(token);
